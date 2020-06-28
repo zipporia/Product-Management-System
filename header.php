f7f98e5d873b397d3868e004aaca04d9d4e2f70f
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-06-28 16:07:12 +0800

    HTML for Singup set

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8" />
     <title>PMS</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <link rel="stylesheet" href="style.css"/>
     <script src="main.js"></script>
 </head>
 <body>
@@ -21,10 +21,17 @@
                 <li><a href="#">Contact</a></li>
             </ul>
             <div>
-                <form action="includes/login.php" method="post"></form>
+                <form action="includes/login.php" method="post">
+                    <input type="text" name="mailuid" placeholder="Username/E-mail...">
+                    <input type="password" name="pwd" placeholder="Password">
+                    <button type="submit" name="login-submit">Login</button>
+                </form>
+                <a href="signup.php">Signup</a>
+                <form action="includes/logout.php" method="post">
+                    <button type="submit" name="logout-submit">Logout</button>
+                </form>
             </div>
         </nav>
-    </header>
-    
+    </header>  
 </body>
 </html>
