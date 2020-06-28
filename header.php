bd643509e1ac7125652ec9f61561895192ec51ca
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-06-28 15:46:11 +0800

    header html data

---
+++ header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>PMS</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+
+    <header>
+        <nav>
+            <a href="">
+                <img src="img/logo.png" alt="logo">
+            </a>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="#">Product</a></li>
+                <li><a href="#">About us</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+            <div>
+                <form action="includes/login.php" method="post"></form>
+            </div>
+        </nav>
+    </header>
+    
+</body>
+</html>
