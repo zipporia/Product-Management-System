a2ec0f8959fb9e6120b7ea76c6106c332b1b441c
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-07-02 22:01:39 +0800

    database added
    signup html added

---
+++ SQL/database.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE loginsystem
+
+CREATE TABLE users (
+    user_id int(11) AUTO_INCREMENT PRIMARY KEY not null,
+    user_uid TINYTEXT NOT NULL,
+    user_email TINYTEXT NOT NULL,
+    user_pwd LONGTEXT NOT NULL,
+);
