44d315e0c0ff146e08252bd66584517dbcd85124
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-07-02 18:16:38 +0800

    body css

--- index.php
+++ index.php
@@ -2,8 +2,12 @@
     require "header.php";
 ?>
     <main>
-        <p>You are logged out!</p>
-        <p>You are logged in!</p>
+        <div class="wrapper-main">
+            <section class="section-default">
+                <p class="login-status">You are logged out!</p>
+                <p class="login-status">You are logged in!</p>
+            </section>
+        </div>
     </main>
 
 
