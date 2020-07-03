a0fb3e31cc2d751f11b5d41f3c7680353f2b92e7
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-07-03 14:55:48 +0800

    signup fixed problems

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -1,2 +1,18 @@
 <?php
 
+if(isset($_POST['signup-submit'])){
+
+    require 'dbh.inc.php';
+
+    $username = $_POST['uid'];
+    $email = $_POST['mail'];
+    $pwd = $_POST['pwd'];
+    $pwd_repeat = $_POST['pwd-repeat'];
+
+
+    if(empty($username) || empty($email) || empty($pwd) || empty($pwd_repeat)){
+        header("Location: ../signup.php?Error=emptyfields&uid=".$username."&email=".$email);
+    }
+
+
+}
--- signup.php
+++ signup.php
@@ -5,7 +5,7 @@
         <div class="wrapper-main">
             <section class="section-default">
                 <h1>Signup</h1>
-                <form action="includes/signup.php" method="post">
+                <form action="includes/signup.inc.php" method="post">
                     <input type="text" name="uid" placeholder="Username">
                     <input type="text" name="mail" placeholder="E-mail">
                     <input type="password" name="pwd" placeholder="Password">
