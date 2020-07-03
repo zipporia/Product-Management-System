00611c6f2920f61df82ea73536d71e4a39d8424a
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-07-03 21:56:28 +0800

    filter var

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -12,7 +12,13 @@ if(isset($_POST['signup-submit'])){
 
     if(empty($username) || empty($email) || empty($pwd) || empty($pwd_repeat)){
         header("Location: ../signup.php?Error=emptyfields&uid=".$username."&email=".$email);
+        exit();
+    }
+    else if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        header("Location: ../signup.php?Error=invalidmail&uid=".$username);
+        exit();
     }
 
+    
 
 }
