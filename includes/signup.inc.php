870be20333175a0202406a447228da170ab7bb22
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-07-04 18:49:42 +0800

    sql insert data

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -27,10 +27,41 @@ if(isset($_POST['signup-submit'])){
         exit();
     }
     else if ($pwd !== $pwd_repeat){
-        header("Location: ../signup.php?Error=passwordcheck&uid=".$username."&mail=".$email);
+        header("Location: ../signup.php?Error=passwordcheck&uid=".$username."&email=".$email);
         exit();
     }
-   
-    
+    else{
+        $sql = "SELECT user_uid FROM users WHERE user_uid=?";
+        $stmt = mysqli_stmt_init($conn);
 
+        if(!mysqli_stmt_prepare($stmt, $sql)){
+            header("Location: ../signup.php?Error=sqlerror");
+            exit();
+        }
+        else{
+            mysqli_stmt_bind_param($stmt, "s" $username);
+            mysqli_stmt_execute($stmt);
+            mysqli_stmt_store_result($stmt);
+            $resutCheck = mysqli_stmt_num_rows($stmt);
+
+            if($resutCheck > 0){
+                header("Location: ../signup.php?Error=usertaken&email=".$email);
+                exit();
+            }
+            else{
+                $sql = "INSERT INTO users (user_uid, user_email, user_pwd) VALUES(?, ?, ?)";
+                $stmt = mysqli_stmt_init($conn);
+
+                if(!mysqli_stmt_prepare($stmt, $sql)){
+                    header("Location: ../signup.php?Error=sqlerror");
+                    exit();
+                }
+                else{
+                    mysqli_stmt_bind_param($stmt, "sss" $username, $email, $pwd);
+                    mysqli_stmt_execute($stmt);
+                    mysqli_stmt_store_result($stmt);
+                }
+            }
+        }
+    }
 }
