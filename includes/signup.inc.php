50b1d70e665d61915a0f6a80c06db3a89919f3e2
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-07-04 14:23:14 +0800

    error handling set

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -14,11 +14,23 @@ if(isset($_POST['signup-submit'])){
         header("Location: ../signup.php?Error=emptyfields&uid=".$username."&email=".$email);
         exit();
     }
+    else if(!filter_var($email, FILTER_VALIDATE_EMAIL) && !preg_match("/^[a-zA-Z0-9]*$/", $username)){
+        header("Location: ../signup.php?Error=invalidmailuid");
+        exit();
+    }
     else if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
         header("Location: ../signup.php?Error=invalidmail&uid=".$username);
         exit();
     }
-
+    else if(!preg_match("/^[a-zA-Z0-9]*$/", $username)){
+        header("Location: ../signup.php?Error=invaliduid&email=".$email);
+        exit();
+    }
+    else if ($pwd !== $pwd_repeat){
+        header("Location: ../signup.php?Error=passwordcheck&uid=".$username."&mail=".$email);
+        exit();
+    }
+   
     
 
 }
